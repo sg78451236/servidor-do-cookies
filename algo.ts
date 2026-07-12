5beb5a7cd8d90ef3d2c51cc0335a790444a35065
sg78451236 <sg78451236@gmail.com>
2026-07-12 16:20:15 -0300

    end point home

--- algo.ts
+++ algo.ts
@@ -5,11 +5,44 @@ const app = express();
 app.use(cors());
 
 
+interface Produto{
+  preco: number,
+  name: string,
+  id: string
+}
+
+//o que tenho que fazer: pegar qnt de cookies (tamanho do array) e atualizar aquela lista lá
+
+const produtos: Produto[] = [
+
+  {preco: 30, name: "Chocolate Brasileiro",  id: '1'},
+  {preco: 20, name: "Chocolate Belga", id: '2'},
+  {preco: 40, name: "Chocolate Frances", id: '3'},
+  {preco: 50, name: "Chocolate Judáico",id: '4'},
+  {preco: 40, name: "Chocolate Ganês", id: '5'},
+  {preco: 40, name: "Chocolate Chines", id: '6'},
+  {preco: 40, name: "Chocolate Holandes Voador", id: '7'},
+  {preco: 40, name: "Brisadeiro", id: '8'},
+  {preco: 40, name: "Brisadeiro de cocaina", id: '9'},
+  {preco: 40, name: "Brisadeiro de dapdoaspdok", id: '10'}
+
+];
+
+app.get('/home', (req,res) => {
+    res.json(produtos)
+}
+)
 
 
-app.get('/produtoid13', (req, res) => {
-  
-    res.json({valor:22})
+app.get('/produto/:id', (req, res) => {
+
+    const id_do_link = req.params.id;
+    for (const element of produtos) {
+      if (element.id === id_do_link) {
+        res.json(element);
+        return;
+    }
+  }
 
 });
 
