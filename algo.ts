5bec11c1a4c872269d0c636810c32cb87479efea
grefano <gabrielnieed@gmail.com>
2026-07-12 12:36:45 -0300

    generate pix qrcode with asaas api

--- algo.ts
+++ algo.ts
@@ -4,6 +4,30 @@ import cors from 'cors'
 const app = express();
 app.use(cors({origin: "http://localhost:5173"}));
 
+async function fetchAsaas(url: string, init?: RequestInit | undefined){
+  if (!process.env.ASAAS_SB_ACCESS_TOKEN){
+    console.log(process.env.ASAAS_SB_ACCESS_TOKEN)
+    throw new Error("environment variable ASAAS_ACCESS_TOKEN not found")
+  }
+  return await fetch(`https://api-sandbox.asaas.com/v3/${url}`, { 
+    headers: {
+      "Content-Type": "application/json",
+      "accept": "application/json",
+      "User-Agent": "cuquis",
+      "access_token": process.env.ASAAS_SB_ACCESS_TOKEN,
+    },
+    ...init,
+  }) 
+
+}
+async function qrcode(){
+  const res = await fetchAsaas("pix/qrCodes/static", {method: 'POST'})
+  console.log(res)
+  const data = await res.json()
+  console.log(data)
+  return data
+}
+
 let databasehorrivel = new Map<string, any>([
   ["cookieirado12309iawd", {
     name: "Cookie Irado",
