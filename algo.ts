c131aaf452119d991ee08fb5d6699f40bd9314d2
sg78451236 <sg78451236@gmail.com>
2026-07-12 16:59:37 -0300

    aalg

--- algo.ts
+++ algo.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import { asaasCreateCustomer, fetchAsaas, qrcodedynamic, qrcodestatic } from './asaas';
+//import { asaasCreateCustomer, fetchAsaas, qrcodedynamic, qrcodestatic } from './asaas';
 const app = express();
 app.use(cors({origin: "http://localhost:5173"}));
 
@@ -20,31 +20,34 @@ let databasehorrivel = new Map<string, Produto>([
   }]
 ])
 
-app.get('/home', (req,res) => {
+app.get('/home', (req:any,res:any) => {
+    console.log(databasehorrivel)
     res.json(databasehorrivel)
 }
 )
 
 
-app.get('/produto/:id', (req, res) => {
+app.get('/produto/:id', (req:any, res:any) => {
     // id = req.query.id
     const id = req.params.id
     console.log("id", id);
     res.json({produto: databasehorrivel.get(id)})
 
 });
+
+
 let _customer: string
 (async () => {
-   _customer = process.env._CUSTOMER ?? ""//await asaasCreateCustomer()
+   //_customer = process.env._CUSTOMER ?? ""//await asaasCreateCustomer()
 })()
-app.post('/pedido/:idProduto', async (req, res) => {
+app.post('/pedido/:idProduto', async (req:any, res:any) => {
   const idProduto = req.params.idProduto
   const produto = databasehorrivel.get(idProduto)
   if (!produto){
     return res.sendStatus(500).send("produto não existe")
   }
-  const pix = await qrcodedynamic(_customer, produto.preco)
-  return res.json({pix})
+  //const pix = await qrcodedynamic(_customer, produto.preco)
+  return 0 //res.json({pix})
 })
 
 app.listen(3000, () => {
