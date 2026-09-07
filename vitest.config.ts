bd23226658d6263c4963253686cc582b0187281e
grefano <gabrielnieed@gmail.com>
2026-09-07 19:40:31 -0300

    vitest include only test files

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,8 @@ import {defineConfig, configDefaults} from 'vitest/config'
 
 export default defineConfig({
   test: {
+    include: ['src/**/*.{test,spec}.{ts,js,tsx,jsx}'],
+    
     exclude: [...configDefaults.exclude, '**/dist/**'],
   }
 })
